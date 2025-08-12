2a9f80b6c9d100565ef649e56f86c709acd5aafa
Walter Arbiol <walterarbiol@gmail.com>
2025-08-12 15:11:34 -0300

    Fix sqlite

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from ui.chat_interface import build_interface
-from config import download_vector_store_if_needed
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+from ui.chat_interface import build_interface
+from config import download_vector_store_if_needed
 
 def main():
     """
